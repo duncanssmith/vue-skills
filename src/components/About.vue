43aab28ed6e7b897d7d49e644817cbdd6750d26a
Duncan Smith <duncanssmith@gmail.com>
2018-07-25 12:00:10 +0100

    Initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ given_name }}, this is the {{ name }} page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  given_name: 'Duncan',
+  data () {
+    return {
+          given_name: this.$route.params.name
+    }
+  }
+}
+</script>
